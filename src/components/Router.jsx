19ae7b5d284f111043b49b11684f4668993f5d44
Riyapathak86 <Ruhiriya86@gmail.com>
2025-02-17 14:24:31 +0530

    Updated Router.

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,7 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import Contact from "./Contact";
-import Experience from "./Education";
+import Education from "./Education";
 import Home from "./Home";
 import App from "./App";
 import Skills from "./Skills";
